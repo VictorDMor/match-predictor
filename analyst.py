c8aca73855b031ebc990e17feccf4bed10015aaa
Victor Moraes <victordm@ciandt.com>
2017-11-20 15:38:03 -0200

    Now computes goals scored and conceded

--- analyst.py
+++ analyst.py
@@ -18,12 +18,16 @@ def getTeamStats(initial_stage, final_stage):
 	home_stats = {
 		'home_wins': 0,
 		'home_draws': 0,
-		'home_losses': 0
+		'home_losses': 0,
+		'home_goals_scored': 0,
+		'home_goals_conceded': 0
 	}
 	away_stats = {
 		'away_wins': 0,
 		'away_draws': 0,
-		'away_losses': 0
+		'away_losses': 0,
+		'away_goals_scored': 0,
+		'away_goals_conceded': 0
 	}
 	for i in range(len(matches_before_round)):
 		if matches_before_round['home_team_api_id'][i] == home_team_id:
@@ -33,6 +37,8 @@ def getTeamStats(initial_stage, final_stage):
 				home_stats['home_draws'] += 1
 			else:
 				home_stats['home_losses'] += 1
+			home_stats['home_goals_scored'] += matches_before_round['home_team_goal'][i]
+			home_stats['home_goals_conceded'] += matches_before_round['away_team_goal'][i]
 		elif matches_before_round['away_team_api_id'][i] == home_team_id:
 			if matches_before_round['away_team_goal'][i] > matches_before_round['home_team_goal'][i]:
 				home_stats['home_wins'] += 1
@@ -40,6 +46,8 @@ def getTeamStats(initial_stage, final_stage):
 				home_stats['home_draws'] += 1
 			else:
 				home_stats['home_losses'] += 1
+			home_stats['home_goals_scored'] += matches_before_round['away_team_goal'][i]
+			home_stats['home_goals_conceded'] += matches_before_round['home_team_goal'][i]
 		if matches_before_round['home_team_api_id'][i] == away_team_id:
 			if matches_before_round['home_team_goal'][i] > matches_before_round['away_team_goal'][i]:
 				away_stats['away_wins'] += 1
@@ -47,6 +55,8 @@ def getTeamStats(initial_stage, final_stage):
 				away_stats['away_draws'] += 1
 			else:
 				away_stats['away_losses'] += 1
+			away_stats['away_goals_scored'] += matches_before_round['home_team_goal'][i]
+			away_stats['away_goals_conceded'] += matches_before_round['away_team_goal'][i]
 		elif matches_before_round['away_team_api_id'][i] == away_team_id:
 			if matches_before_round['away_team_goal'][i] > matches_before_round['home_team_goal'][i]:
 				away_stats['away_wins'] += 1
@@ -54,6 +64,26 @@ def getTeamStats(initial_stage, final_stage):
 				away_stats['away_draws'] += 1
 			else:
 				away_stats['away_losses'] += 1
+			away_stats['away_goals_scored'] += matches_before_round['away_team_goal'][i]
+			away_stats['away_goals_conceded'] += matches_before_round['home_team_goal'][i]
+
+	home_points = home_stats['home_wins']*3+home_stats['home_draws']
+	away_points = away_stats['away_wins']*3+away_stats['away_draws']
+
+	print(home_team + " stats:")
+	print("Wins: " + str(home_stats['home_wins']))
+	print("Draws: " + str(home_stats['home_draws']))
+	print("Losses: " + str(home_stats['home_losses']))
+	print("Points: " + str(home_points) + "\n")
+	print("Goals scored: " + str(home_stats['home_goals_scored']))
+	print("Goals conceded: " + str(home_stats['home_goals_conceded']) + "\n")
+	print(away_team + " stats:")
+	print("Wins: " + str(away_stats['away_wins']))
+	print("Draws: " + str(away_stats['away_draws']))
+	print("Losses: " + str(away_stats['away_losses']))
+	print("Points: " + str(away_points) + "\n")
+	print("Goals scored: " + str(away_stats['away_goals_scored']))
+	print("Goals conceded: " + str(away_stats['away_goals_conceded']) + "\n")
 
 	return([home_stats, away_stats])
 
@@ -76,38 +106,14 @@ match = pd.read_sql_query("select * from Match where home_team_api_id = "+ str(h
 match_round = match['stage'][0]
 
 # Step 3: Get team stats until that round
-[home_stats, away_stats] = getTeamStats(1, match_round)
-
-home_points = home_stats['home_wins']*3+home_stats['home_draws']
-away_points = away_stats['away_wins']*3+away_stats['away_draws']
-
-print(home_team + " stats:")
-print("Wins: " + str(home_stats['home_wins']))
-print("Draws: " + str(home_stats['home_draws']))
-print("Losses: " + str(home_stats['home_losses']))
-print("Points: " + str(home_points) + "\n")
-print(away_team + " stats:")
-print("Wins: " + str(away_stats['away_wins']))
-print("Draws: " + str(away_stats['away_draws']))
-print("Losses: " + str(away_stats['away_losses']))
-print("Points: " + str(away_points) + "\n")
-
+getTeamStats(1, match_round)
 ####################
 # Last seven games #
 ####################
 
-[home_last_seven, away_last_seven] = getTeamStats(match_round-7, match_round)
+getTeamStats(match_round-7, match_round)
 
-home_points_last_seven = home_last_seven['home_wins']*3+home_last_seven['home_draws']
-away_points_last_seven = away_last_seven['away_wins']*3+away_last_seven['away_draws']
+#############################
+# Attack/Defense efficiency #
+#############################
 
-print(home_team + " stats (Last seven rounds):")
-print("Wins: " + str(home_last_seven['home_wins']))
-print("Draws: " + str(home_last_seven['home_draws']))
-print("Losses: " + str(home_last_seven['home_losses']))
-print("Points: " + str(home_points_last_seven) + "\n")
-print(away_team + " stats (Last seven rounds):")
-print("Wins: " + str(away_last_seven['away_wins']))
-print("Draws: " + str(away_last_seven['away_draws']))
-print("Losses: " + str(away_last_seven['away_losses']))
-print("Points: " + str(away_points_last_seven) + "\n")
